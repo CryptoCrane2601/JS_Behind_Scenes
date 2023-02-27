de7c107e663f4ab606d4b98ef5bee93e2fa99d28
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-02-27 23:29:54 +0100

    out of scope examples

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ function calcAge(birthYear) {
   function printAge() {
     const output = `You are ${age} years old, born in ${birthYear}.`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, you are millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str); // out of scope
+    console.log(millenial);
+    // add(2, 3); // out of scope
   }
   printAge();
 
