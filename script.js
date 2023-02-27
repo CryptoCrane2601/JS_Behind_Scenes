11ea0a8dcdb740bd6b6234605529cfce41dbe82d
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-02-27 23:54:01 +0100

    hoisting practice

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
-'use strict';
+// 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2023 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2023 - birthYear;
 
-  function printAge() {
-    const output = `You are ${age} years old, born in ${birthYear}.`;
-    console.log(output);
+//   function printAge() {
+//     let output = `You are ${age} years old, born in ${birthYear}.`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      const str = `Oh, you are millenial, ${firstName}`;
-      console.log(str);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       const firstName = 'Katarina';
+//       const str = `Oh, you are millenial, ${firstName}`;
+//       console.log(str);
+//       output = 'NEW OUTPUT!'; // Reassining outer's scope variable
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-    // console.log(str); // out of scope
-    console.log(millenial);
-    // add(2, 3); // out of scope
-  }
-  printAge();
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     // console.log(str); // out of scope
+//     console.log(millenial);
+//     // add(2, 3); // out of scope
+//     console.log(output);
+//   }
+//   printAge();
 
-  return age;
-}
+//   return age;
+// }
 
-const firstName = 'Dejan';
-calcAge(1989);
+// const firstName = 'Dejan';
+// calcAge(1989);
+
+// HOISTING PRACTICE:
+
+console.log(me);
+console.log(job);
+console.log(year);
+
+var me = 'Dejan';
+let job = 'Salesman';
+const year = 1989;
